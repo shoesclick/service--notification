df0545749f230e7ab59a5f3a1c2da792340894f0
git <claytnmorais@gmail.com>
2024-03-24 02:53:35 -0300

    apis com integração com keycloak

--- src/main/java/com/shoesclick/service/notification/repository/TokenRepository.java
+++ src/main/java/com/shoesclick/service/notification/repository/TokenRepository.java
@@ -1,18 +1,21 @@
 package com.shoesclick.service.notification.repository;
 
 import com.shoesclick.service.notification.entity.SsoToken;
-import feign.Headers;
-import feign.Param;
-import feign.RequestLine;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Map;
 
 @FeignClient(value = "tokenClient", url = "${token.tokenUrl}")
 public interface TokenRepository {
 
 
-    @RequestLine("POST /protocol/openid-connect/token")
-    @Headers("Content-Type: application/x-www-form-urlencoded")
-    SsoToken getToken(@Param("grant_type") String grantType, @Param("client_id") String clientId, @Param("client_secret") String clientSecret);
+    @RequestMapping(method = RequestMethod.POST, value = "/protocol/openid-connect/token", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = "application/json")
+    SsoToken getToken(@RequestBody MultiValueMap<String, String> formToken);
 
 
 }
--- src/test/java/com/shoesclick/service/notification/service/TokenServiceTest.java
+++ src/test/java/com/shoesclick/service/notification/service/TokenServiceTest.java
@@ -2,6 +2,7 @@ package com.shoesclick.service.notification.service;
 
 import com.shoesclick.service.notification.config.properties.TokenProperties;
 import com.shoesclick.service.notification.entity.SsoToken;
+import com.shoesclick.service.notification.mapper.TokenMapper;
 import com.shoesclick.service.notification.repository.TokenRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,10 +10,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.util.LinkedMultiValueMap;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -23,6 +28,9 @@ class TokenServiceTest {
 
     @Mock
     private TokenProperties tokenProperties;
+
+    @Mock
+    private TokenMapper tokenMapper;
 
     @InjectMocks
     private TokenService tokenService;
@@ -37,14 +45,16 @@ class TokenServiceTest {
 
     @Test
     void shouldGetTokenSuccess(){
-        when(tokenRepository.getToken(anyString(),anyString(),anyString())).thenReturn(new SsoToken());
+        when(tokenRepository.getToken(any())).thenReturn(new SsoToken());
+        when(tokenMapper.map(any(TokenProperties.class))).thenReturn(new LinkedMultiValueMap<>());
         var token = tokenService.getToken();
         assertNotNull(token);
     }
 
     @Test
     void shouldGetTokenIsNull(){
-        when(tokenRepository.getToken(anyString(),anyString(),anyString())).thenReturn(null);
+        when(tokenRepository.getToken(any())).thenReturn(null);
+        when(tokenMapper.map(any(TokenProperties.class))).thenReturn(new LinkedMultiValueMap<>());
         var token = tokenService.getToken();
         assertNull(token);
     }
