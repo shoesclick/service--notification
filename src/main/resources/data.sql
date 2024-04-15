3d7f7bd42e260972f04a4c075998ac0ad5cb85f7
git <claytnmorais@gmail.com>
2024-04-15 19:42:05 -0300

    ajuste no metodo da pipeline

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into template_email(typeTemplate,subject,body) values ('CREATE_ORDER','Seu Pedido Foi criado','Seu pedido foi criado com sucesso')
+insert into template_email(typeTemplate,subject,body) values ('PAYMENT_APPROVED','Seu Pagamento foi Aprovado','Aprovamos o seu pagamento')
+insert into template_email(typeTemplate,subject,body) values ('PAYMENT_NOT_AUTHORIZED','Seu Pagamento foi Recusado','Recusamos o seu pagamento')
+insert into template_email(typeTemplate,subject,body) values ('PIX_GENERATED','Pix Gerado','Efetue o pagamento com sua chave pix')
+insert into template_email(typeTemplate,subject,body) values ('TICKET_GENERATED','Boleto Gerado','Segue o código de barras para o pagamento')
+
+
